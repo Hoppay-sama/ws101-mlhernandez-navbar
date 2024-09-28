5d108827f6e7d350d2fa8a21b1ef7de6946daaa9
Hoppay-sama <morijin1420@gmail.com>
2024-09-28 19:03:19 +0800

    Updates

---
+++ Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div>
+            <video src={"../Maitake.mp4"} type="video/mp4" autoplay loop muted>
+            </video>
+        </div>
+    )
+}
